208d8e57310d42ff575d2fbb42d43084bdd9b1ac
Oscar Mesejo <oscar.mesejo@gmail.com>
2023-08-24 17:44:29 +0200

    footer css improved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
+import "./../styles/Footer.css"
+
 function Footer(){
     return(
-        <div>
-            <div>
+        <div className="footer">
+            
+            <div className="logos">
                 <img width = "50px" src="../../images/twitter logo.png" alt="" />
-            </div>
-            <div>
                 <img width = "50px" src="../../images/facebook logo.png" alt="" />
-            </div>
-            <div>
                 <img width = "50px" src="../../images/instagram logo.png" alt="" />
             </div>
+            
 
             <h2>Info | Support | Contact</h2>
             <h3>Terms of Use | Privacy Policy</h3>
