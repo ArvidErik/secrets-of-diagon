ffce136e7cb642307047616829dfbd4d5693e648
ArvidErik <erik.arvid.hosszu@gmail.com>
2023-08-24 17:58:00 +0200

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,16 @@
+import sample from "../assets/video.mp4"
+import "../styles/Navbar.css"
+
 function Hero () {
     return (
         <div className="hero">
-            <video src=""></video>
-            <div>
-                <h1>HERO</h1>
+            <video className='videoTag' autoPlay loop muted>
+                <source src={sample} type='video/mp4' />
+            </video>
+
+            <div className="hero-text">
+                <h1 className="hero-text">BOMBARDA <br/>MAXIMA</h1>
+                <button className="hero-button">Discover our products</button>
             </div>
         </div>
     )
