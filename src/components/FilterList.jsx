a1d9f8e085af66798b6a3140e7976f4bba9726c0
Rodricancio8 <rodri.cancio@hotmail.com>
2023-08-24 18:07:14 +0200

    working in style

--- src/components/FilterList.jsx
+++ src/components/FilterList.jsx
@@ -1,16 +1,19 @@
-import { useState, } from "react";
+import NameCard from "./NameCard";
+import "../styles/FilterList.css"
 
 function FilterList() {
     const Movie = ["Harry Potter", "Hermione", "Dumbledore", "Ron Weasley", "Voldemort", "See All"];
-    const [names, setNames] = useState([]);
+
 
     return (
         <>
             <h1>NAMES</h1>
+            <div className="card-container">
+                {Movie.map((name, index) => (
+                    <NameCard name={name} key={index} />
+                ))}
 
-            {names.map((name, index) => (
-                <NameCard NameObject={name} key={index} />
-            ))}
+            </div>
         </>
     );
 }
---
+++ src/components/NameCard.jsx
@@ -0,0 +1,13 @@
+import "../styles/FilterList.css"
+
+function NameCard(props){
+
+    return (
+<div className="name-card">
+            <h2>{props.name} </h2>
+        </div>
+    
+    )
+}
+
+export default NameCard
