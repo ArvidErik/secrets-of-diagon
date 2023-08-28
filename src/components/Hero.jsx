8c8bfcc4e0cc9121b592a479b14ae947ffe41076
Rodricancio8 <rodri.cancio@hotmail.com>
2023-08-28 16:51:09 +0200

    style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import "../styles/Navbar.css"
 function Hero () {
     return (
         <div className="hero">
-                <h1 className="hero-text">BOMBARDA <br/>MAXIMA</h1>
+                <h1 className="hero-text">Bombarda <br/>Maxima</h1>
                 <button className="hero-button">Discover our products</button>
         </div>
     )
